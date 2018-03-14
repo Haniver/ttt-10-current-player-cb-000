a8c403016b9acc2c3ef6ff1acb0c0a374e635f91
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-03-14 14:13:35 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   occupied = 0
   board.each do |position|
-    if !position == "" && !position == " "
+    if position != "" && position != " "
       occupied += 1
     end
   end
